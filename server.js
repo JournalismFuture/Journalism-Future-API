47fadf3b7f526499ad6d15826c0f09774019f810
Leon Erath <leon-erath@hotmail.de>
2018-08-08 18:57:01 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+var os = require("os");
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function(ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function(iface) {
+    if ("IPv4" !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ":" + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
